f0d61d70bdd491ef498c1f8c80e414247a53317a
Daniel Sanchez <dsan@mit.edu>
2014-01-19 21:06:02 -0500

    more work on profile page

--- ProfilePage.js
+++ ProfilePage.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	$("html").height($(window).height());
+	$(window).resize(function() {
+		$("html").height($(window).height());
+	});
+
+	$("#totalDistIcon").tooltip({"placement": "right"});
+	$("#totalCalIcon").tooltip({"placement": "right"});
+
+	$("#goalsPlus").on("click", function() {
+		var buttonHeight = parseInt($("#newGoalBtn").css("height")) + parseInt($("#newGoalContainer").css("padding")) + parseInt($("#newGoalContainer").css("padding"));
+		$("#newGoalContainer").css("border-top", "1px solid grey");
+		$("#newGoalContainer").animate({"height": buttonHeight, "padding-bottom": "5px"}, 300);
+	});
 });
