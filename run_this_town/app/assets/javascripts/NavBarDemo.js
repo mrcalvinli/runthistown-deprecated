644926717f50d6c90c6f970d1498be23ab51e892
Calvin Li <calvinli@mit.edu>
2014-01-21 00:30:23 -0500

    changed image location

---
+++ run_this_town/app/assets/javascripts/NavBarDemo.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	var extraInfohtml = '<ul class="dropdown-menu" role="menu" style = "display: block; position: relative; border: none; box-shadow: none; margin-top: -8px;">'
+		+ '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Help</a></li>'
+		+ '<li role="presentation" class="divider"></li>'
+	    + '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Profile</a></li>'
+	    + '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Account Settings</a></li>'
+	    + '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Log Out</a></li>'
+	    + '<li role="presentation" class="divider"></li>'
+	    + '<li role="presentation"><a id = "creditsBtn" role="menuitem" tabindex="-1" href="#" data-toggle="modal" data-target="#creditsModal">Credits</a></li>'
+	+ '</ul>';
+	$("#extraInfo").popover({'placement': 'bottom', 'html': true, 'content': extraInfohtml});
+	$("#extraInfo").on("click", function() {
+		console.log("swag");
+		var popoverRight = $(".popover").offset().left + $(".popover").width();
+		if ( popoverRight > $(window).width() ) {
+			var difference = popoverRight - $(window).width();
+			$(".popover").css("left",  parseInt($(".popover").css("left")) - difference - 10 );
+			$(".arrow").css("left", parseInt($(".arrow").css("left")) + difference + 10);
+		}
+	});
+});
