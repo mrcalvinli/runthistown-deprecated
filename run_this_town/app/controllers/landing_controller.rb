ac9ec572b911c7080d3093bbb2a711f1eb79e08f
Mata <christianmata@Christians-MacBook-Pro.local>
2014-01-22 00:20:56 -0500

    Finalized the forms and added the additional fields to the database

--- run_this_town/app/controllers/landing_controller.rb
+++ run_this_town/app/controllers/landing_controller.rb
@@ -1,4 +1,13 @@
 class LandingController < ApplicationController
+
 	def home
+	end
+
+	private
+	#redirects user if they are logged in
+	def logged_in
+	  if current_user != nil
+	    redirect_to homepage
+	  end
 	end
 end
