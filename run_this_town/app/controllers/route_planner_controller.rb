275425093d605513c729625c51390d60234eed26
Calvin Li <calvinli@mit.edu>
2014-01-22 21:46:40 -0500

    took out angelalum, fixed font on home page

--- run_this_town/app/controllers/route_planner_controller.rb
+++ run_this_town/app/controllers/route_planner_controller.rb
@@ -10,14 +10,16 @@ class RoutePlannerController < ApplicationController
 
 			#Save info into route
 			locDic = params
-			@route.user_id = locDic["user_id"]
-			@route.name = locDic["name"]
-			locDic["locations"].each do |key, locInfo|
+			@route.user_id = current_user.id
+			@route.name = nil #Add name if needed
+			(0..(locDic["locations"].size - 1)).each do |i|
+				locInfo = locDic["locations"][i.to_s]
+				p locInfo
 				@route.add_new_loc(locInfo[0], locInfo[1], locInfo[2])
 			end
 			@route.save
-			#format.html { redirect_to root_path }
-			#format.json { render json: { :redirect => root_url } }
+			format.html { redirect_to homepage_path }
+			format.json { render json: { :redirect => homepage_url } } 
 		end
 	end
 end
