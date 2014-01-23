942ece2ca1b2be6f147758fb55f470788cca1664
Daniel Sanchez <dsan@mit.edu>
2014-01-23 00:39:28 -0500

    changing pageLoad

--- run_this_town/app/assets/javascripts/RoutePlanner.js
+++ run_this_town/app/assets/javascripts/RoutePlanner.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function pageLoad() {
 	/**** Styling JavaScript ***/
 	$("#map").css("height", ($(window).height() - $(".navbar").height));
 	console.log("mapHeight: " + $("#map").height());
@@ -540,4 +540,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-});
+}
+
+$(document).on("page:load", pageLoad);
+$(document).ready(pageLoad);
