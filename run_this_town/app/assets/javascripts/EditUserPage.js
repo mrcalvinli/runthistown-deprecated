d9667ee57adfef0d47ef3f02bf27fad78c5b676c
Mata <cmata@mit.edu>
2014-01-28 07:02:28 -0500

    Added changes for final two js pages and compiled assets

--- run_this_town/app/assets/javascripts/EditUserPage.js
+++ run_this_town/app/assets/javascripts/EditUserPage.js
@@ -1,16 +1,20 @@
-// function editUserPageLoad() {
-//     if ($("#currentPasswordEdit").val() == "") {
-//       document.getElementById("updateProfBtn").disabled = true;
-//     }
-//     $("#currentPasswordEdit").bind("propertychange keyup input paste", function() {
-//       if ($("#currentPasswordEdit").val() == "") {
-//         document.getElementById("updateProfBtn").disabled = true;
-//       } else {
-//         document.getElementById("updateProfBtn").disabled = false;
-//       }
-//     });
 
-//   }
-//   $(document).on("page:load", editUserPageLoad);
-//   $(document).ready(editUserPageLoad);
+function editUserPageLoad() {
+  if (document.getElementById("registrationForm") != null) {
+    if ($("#currentPasswordEdit").val() == "") {
+      document.getElementById("updateProfBtn").disabled = true;
+    }
+    $("#currentPasswordEdit").bind("propertychange keyup input paste", function() {
+      if ($("#currentPasswordEdit").val() == "") {
+        document.getElementById("updateProfBtn").disabled = true;
+      } else {
+        document.getElementById("updateProfBtn").disabled = false;
+      }
+    });
+
+  }
+}
+
+$(document).on("page:load", editUserPageLoad);
+$(document).ready(editUserPageLoad);
 
