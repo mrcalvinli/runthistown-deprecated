78237a131590d4ff401d0fd3c86084dd1f7e69fa
Calvin Li <calvinli@mit.edu>
2014-01-28 06:32:50 -0500

    weight added (RAKE DB;MIGRATE) to user

--- run_this_town/db/schema.rb
+++ run_this_town/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127085639) do
+ActiveRecord::Schema.define(version: 20140128112726) do
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140127085639) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.float    "weight"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
