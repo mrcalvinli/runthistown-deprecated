a02e1289f5edb8027c997a3375327a1860a1239e
Mata <christianmata@dhcp-18-111-24-105.dyn.mit.edu>
2014-01-27 21:50:38 -0500

    Try commenting out other js

--- run_this_town/app/assets/javascripts/ProfilePage.js
+++ run_this_town/app/assets/javascripts/ProfilePage.js
@@ -1,233 +1,233 @@
-function pageLoad() {
-	// Backend Stuff
-	// $("#name").html("Myname Mcgee");
+// function pageLoad() {
+// 	// Backend Stuff
+// 	// $("#name").html("Myname Mcgee");
 
-	// Styling Javascript
-	/*$("html").height($(window).height());*/
+// 	// Styling Javascript
+// 	/*$("html").height($(window).height());*/
 
-	/*$(window).resize(function() {
-		$("html").height($(window).height());
-	});*/
+// 	/*$(window).resize(function() {
+// 		$("html").height($(window).height());
+// 	});*/
 
-	// Functional JavaScript
+// 	// Functional JavaScript
 
-	$("#totalDistIcon").tooltip({"placement": "right"});
-	$("#totalCalIcon").tooltip({"placement": "right"});
-	$("#longestRunIcon").tooltip({"placement": "right"});
+// 	$("#totalDistIcon").tooltip({"placement": "right"});
+// 	$("#totalCalIcon").tooltip({"placement": "right"});
+// 	$("#longestRunIcon").tooltip({"placement": "right"});
 
-	$("#goalsPlus").on("click", function() {
-		var buttonHeight = parseInt($("#newGoalBtn").css("height")) + parseInt($("#newGoalContainer").css("padding")) + parseInt($("#newGoalContainer").css("padding"));
-		$("#newGoalContainer").css("border-top", "1px solid grey");
-		$("#newGoalContainer").animate({"height": buttonHeight, "padding-bottom": "5px"}, 300);
-	});
+// 	$("#goalsPlus").on("click", function() {
+// 		var buttonHeight = parseInt($("#newGoalBtn").css("height")) + parseInt($("#newGoalContainer").css("padding")) + parseInt($("#newGoalContainer").css("padding"));
+// 		$("#newGoalContainer").css("border-top", "1px solid grey");
+// 		$("#newGoalContainer").animate({"height": buttonHeight, "padding-bottom": "5px"}, 300);
+// 	});
 
-	var totalDistanceTraveled = 0;
-	$("#routesToRunContainer .profRouteEntry .profRouteDistanceVal").each(function(i) {
-		if ($(this).html() != "") {
-			console.log($(this).html());
-			totalDistanceTraveled += parseFloat($(this).html());
-		}
-		console.log(totalDistanceTraveled);
-		$("#totalDistanceStat").html(totalDistanceTraveled.toString() + " mi");
-	});
+// 	var totalDistanceTraveled = 0;
+// 	$("#routesToRunContainer .profRouteEntry .profRouteDistanceVal").each(function(i) {
+// 		if ($(this).html() != "") {
+// 			console.log($(this).html());
+// 			totalDistanceTraveled += parseFloat($(this).html());
+// 		}
+// 		console.log(totalDistanceTraveled);
+// 		$("#totalDistanceStat").html(totalDistanceTraveled.toString() + " mi");
+// 	});
 
-	var longestRun = 0;
-	$("#routesToRunContainer .profRouteEntry .profRouteDistanceVal").each(function(i) {
-		if ($(this).html() != "") {
-			console.log($(this).html());
-			thisDistance = parseFloat($(this).html());
-			if (thisDistance > longestRun) {
-				longestRun = thisDistance;
-			}
-		}
-		$("#longestRunStat").html(longestRun.toString() + " mi");
-	});
+// 	var longestRun = 0;
+// 	$("#routesToRunContainer .profRouteEntry .profRouteDistanceVal").each(function(i) {
+// 		if ($(this).html() != "") {
+// 			console.log($(this).html());
+// 			thisDistance = parseFloat($(this).html());
+// 			if (thisDistance > longestRun) {
+// 				longestRun = thisDistance;
+// 			}
+// 		}
+// 		$("#longestRunStat").html(longestRun.toString() + " mi");
+// 	});
 
-	// Route entry template
+// 	// Route entry template
 
-	$(".profRouteEntry").on("click", function() {
-		if ( $(this).css("height") == "40px" ) {
-			$(this).css("height", "auto");
-		} else {
-			$(this).css("height", "40px");
-		}
-	});
+// 	$(".profRouteEntry").on("click", function() {
+// 		if ( $(this).css("height") == "40px" ) {
+// 			$(this).css("height", "auto");
+// 		} else {
+// 			$(this).css("height", "40px");
+// 		}
+// 	});
 
 	
 
-	$("#profPicCont").on("mouseenter", function() {
-		$("#profPicCover").css("opacity", 1);
-	});
-	$("#profPicCont").on("mouseleave", function() {
-		$("#profPicCover").css("opacity", 0);
-	});
+// 	$("#profPicCont").on("mouseenter", function() {
+// 		$("#profPicCover").css("opacity", 1);
+// 	});
+// 	$("#profPicCont").on("mouseleave", function() {
+// 		$("#profPicCover").css("opacity", 0);
+// 	});
 
-	if ($("#routesRunContainer").html() == "") {
-		$("#routesRunContainer").append($("<div style = 'height: 100%; text-align: center; '>You have no routes yet.  Click the button above to get started!</div>"));
-	}
+// 	if ($("#routesRunContainer").html() == "") {
+// 		$("#routesRunContainer").append($("<div style = 'height: 100%; text-align: center; '>You have no routes yet.  Click the button above to get started!</div>"));
+// 	}
 
-	//deleteRouteLink
-	$(".deleteRouteLink").append($('<span id = "profDeleteRoute" style = "color: #e6463d; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-remove profDeleteRoute"></span>'));
+// 	//deleteRouteLink
+// 	$(".deleteRouteLink").append($('<span id = "profDeleteRoute" style = "color: #e6463d; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-remove profDeleteRoute"></span>'));
 	
-	$("#profDeleteRoute").on("mouseenter", function() {
-		$(this).css("opacity", 1);
-	});
+// 	$("#profDeleteRoute").on("mouseenter", function() {
+// 		$(this).css("opacity", 1);
+// 	});
 
-	$("#profDeleteRoute").on("mouseleave", function() {
-		$(this).css("opacity", 0.5);
-	});
+// 	$("#profDeleteRoute").on("mouseleave", function() {
+// 		$(this).css("opacity", 0.5);
+// 	});
 
 
-	// Backend Stuff
-	// New Stat template
-		// $(".statContainer").append('<div class = "stat"><p class = "statVal">Stat goes here</p><span class = "glyphicon glyphicon-search"></span></div>');
-	// New Route Template
-		/*$(".routesRunContainer").append(
-			'<div class = "profRouteEntry well well-sm">
-						<div style = "display: inline-block; width: 49%; ">
-							<p>Date: </p>
-							<p>Distance Traveled: </p>
-							<p>Start: </p>
-							<p>End: </p>
-						</div>
-						<div style = "display: inline-block; width: 49%; height: 100%; position: absolute; top: 0; ">
-							<legend><h4 style = "text-align: center; font-family: Sanchez Regular">Waypoints Visited:</h4></legend>
-							<div class = "wptsList">
-								<ol>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-									<li>Place</li>
-								</ol>
-							</div>
-						</div>
-					</div>'
-		)*/
+// 	// Backend Stuff
+// 	// New Stat template
+// 		// $(".statContainer").append('<div class = "stat"><p class = "statVal">Stat goes here</p><span class = "glyphicon glyphicon-search"></span></div>');
+// 	// New Route Template
+// 		/*$(".routesRunContainer").append(
+// 			'<div class = "profRouteEntry well well-sm">
+// 						<div style = "display: inline-block; width: 49%; ">
+// 							<p>Date: </p>
+// 							<p>Distance Traveled: </p>
+// 							<p>Start: </p>
+// 							<p>End: </p>
+// 						</div>
+// 						<div style = "display: inline-block; width: 49%; height: 100%; position: absolute; top: 0; ">
+// 							<legend><h4 style = "text-align: center; font-family: Sanchez Regular">Waypoints Visited:</h4></legend>
+// 							<div class = "wptsList">
+// 								<ol>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 									<li>Place</li>
+// 								</ol>
+// 							</div>
+// 						</div>
+// 					</div>'
+// 		)*/
 
-/*	function successClick(current) {
-		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
-		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
-		var wptAddresses = [];
-		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
-			wptAddresses.push(current.html());
-		});
-		console.log(startAddress, endAddress, wptAddresses);
+// 	function successClick(current) {
+// 		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
+// 		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
+// 		var wptAddresses = [];
+// 		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
+// 			wptAddresses.push(current.html());
+// 		});
+// 		console.log(startAddress, endAddress, wptAddresses);
 		
-		var currentRoute = current.parent().parent();
-		current.remove();
-		currentRoute.children(".profConfirmOrDeny").children(".profDeleteRoute").attr("id", "profRemoveRoute");
+// 		var currentRoute = current.parent().parent();
+// 		current.remove();
+// 		currentRoute.children(".profConfirmOrDeny").children(".profDeleteRoute").attr("id", "profRemoveRoute");
 		
-		currentRoute.remove();
-		$("#routesRunContainer").append(currentRoute);
+// 		currentRoute.remove();
+// 		$("#routesRunContainer").append(currentRoute);
 
-		$(".profRouteEntry").on("click", function() {
-			if ( $(this).css("height") == "40px" ) {
-				$(this).css("height", "auto");
-			} else {
-				$(this).css("height", "40px");
-			}
-		});
+// 		$(".profRouteEntry").on("click", function() {
+// 			if ( $(this).css("height") == "40px" ) {
+// 				$(this).css("height", "auto");
+// 			} else {
+// 				$(this).css("height", "40px");
+// 			}
+// 		});
 
-		$("#profRemoveRoute").on("click", function() {
-			removeClick($(this));
-		});
+// 		$("#profRemoveRoute").on("click", function() {
+// 			removeClick($(this));
+// 		});
 
-		// backend stuff goes here, use startAddress, endAddress, and wptAddresses //
-	}*/
+// 		// backend stuff goes here, use startAddress, endAddress, and wptAddresses //
+// 	}
 
 	
-	/*function deleteClick(current) {
-		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
-		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
-		var wptAddresses = [];
-		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
-			wptAddresses.push($(this).html());
-		});
-		console.log(wptAddresses);
-		var currentRoute = current.parent().parent();
+// 	/*function deleteClick(current) {
+// 		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
+// 		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
+// 		var wptAddresses = [];
+// 		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
+// 			wptAddresses.push($(this).html());
+// 		});
+// 		console.log(wptAddresses);
+// 		var currentRoute = current.parent().parent();
 
-		currentRoute.remove();
+// 		currentRoute.remove();
 
-		$("#profDeleteRoute").on("click", function() {
-			deleteClick($(this));
-		}); */
+// 		$("#profDeleteRoute").on("click", function() {
+// 			deleteClick($(this));
+// 		}); */
 
-		/*$(".profRouteEntry").on("click", function(route) {
-			if ( route.css("height") == "40px" ) {
-				route.css("height", "auto");
-			} else {
-				route.css("height", "40px");
-			}
-		});*/
+// 		/*$(".profRouteEntry").on("click", function(route) {
+// 			if ( route.css("height") == "40px" ) {
+// 				route.css("height", "auto");
+// 			} else {
+// 				route.css("height", "40px");
+// 			}
+// 		});*/
 
-		// backend stuff goes here, use startAddress, endAddress, and wptAddresses
-		/*$.ajax({
-			beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-			url: '/homepage_post',
-			type: 'POST',
-			data: {
-				"start": startAddress,
-				"end": endAddress,
-				"wptAddress": wptAddresses
-			},
-			dataType: "json",
-			success: function(data, textStatus){
-				console.log("deletion twerked")
-			},
-			error: function(){
-				console.log("did not delete...");
-			}
-		}); 
+// 		// backend stuff goes here, use startAddress, endAddress, and wptAddresses
+// 		/*$.ajax({
+// 			beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+// 			url: '/homepage_post',
+// 			type: 'POST',
+// 			data: {
+// 				"start": startAddress,
+// 				"end": endAddress,
+// 				"wptAddress": wptAddresses
+// 			},
+// 			dataType: "json",
+// 			success: function(data, textStatus){
+// 				console.log("deletion twerked")
+// 			},
+// 			error: function(){
+// 				console.log("did not delete...");
+// 			}
+// 		}); 
 
-	} */
+// 	} */
 
-/*	function removeClick(current) {
-		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
-		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
-		var wptAddresses = [];
-		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
-			wptAddresses.push(current.html());
-		});
-		console.log(startAddress, endAddress, wptAddresses);
-		var currentRoute = current.parent().parent();
-		currentRoute.children(".profConfirmOrDeny").html('<span id = "profSuccessRoute" style = "color: #2eba3e; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-ok profConfirmRoute"></span><span id = "profDeleteRoute" style = "color: #e6463d; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-remove profDeleteRoute"></span>');
-		currentRoute.remove();
-		$("#routesToRunContainer").append(currentRoute);
+// /*	function removeClick(current) {
+// 		var startAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteStart").children("span").html();
+// 		var endAddress = current.parent().parent().children(".profRunRouteInfo").children(".profRouteEnd").children("span").html();
+// 		var wptAddresses = [];
+// 		current.parent().parent().children(".profWaypointsContainer").children(".wptsList").children("ol").children("li").each(function() {
+// 			wptAddresses.push(current.html());
+// 		});
+// 		console.log(startAddress, endAddress, wptAddresses);
+// 		var currentRoute = current.parent().parent();
+// 		currentRoute.children(".profConfirmOrDeny").html('<span id = "profSuccessRoute" style = "color: #2eba3e; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-ok profConfirmRoute"></span><span id = "profDeleteRoute" style = "color: #e6463d; font-size: 20px; opacity: 0.5" class = "glyphicon glyphicon-remove profDeleteRoute"></span>');
+// 		currentRoute.remove();
+// 		$("#routesToRunContainer").append(currentRoute);
 
-		$(".profRouteEntry").on("click", function() {
-			if ( $(this).css("height") == "40px" ) {
-				$(this).css("height", "auto");
-			} else {
-				$(this).css("height", "40px");
-			}
-		});
+// 		$(".profRouteEntry").on("click", function() {
+// 			if ( $(this).css("height") == "40px" ) {
+// 				$(this).css("height", "auto");
+// 			} else {
+// 				$(this).css("height", "40px");
+// 			}
+// 		});
 
-		$("#profSuccessRoute").on("click", function() {
-			successClick($(this));
-		});
+// 		$("#profSuccessRoute").on("click", function() {
+// 			successClick($(this));
+// 		});
 
-		$("#profDeleteRoute").on("click", function() {
-			deleteClick($(this));
-		});
+// 		$("#profDeleteRoute").on("click", function() {
+// 			deleteClick($(this));
+// 		});
 
-		// backend stuff goes here, use startAddress, endAddress, and wptAddresses
-	}*/
-/*	$("#profSuccessRoute").on("click", function() {
-		successClick($(this));
-	});*/
+// 		// backend stuff goes here, use startAddress, endAddress, and wptAddresses
+// 	}*/
+// /*	$("#profSuccessRoute").on("click", function() {
+// 		successClick($(this));
+// 	});*/
 
-	/*$("#profDeleteRoute").on("click", function() {
-		deleteClick($(this));
-	});*/
+// 	/*$("#profDeleteRoute").on("click", function() {
+// 		deleteClick($(this));
+// 	});*/
 
-/*	$("#profRemoveRoute").on("click", function() {
-		removeClick($(this));
-	});*/
-}
+// /*	$("#profRemoveRoute").on("click", function() {
+// 		removeClick($(this));
+// 	});*/
+// }
 
-$(document).on("page:load", pageLoad);
-$(document).ready(pageLoad);
+// $(document).on("page:load", pageLoad);
+// $(document).ready(pageLoad);
